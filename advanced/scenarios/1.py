1af10d607f9501aa067368b98cd33e758c61453c
vagrant <vagrant@i-am-become-penguin>
2018-02-18 22:08:49 +0000

    work on scenario 1

--- advanced/scenarios/1.py
+++ advanced/scenarios/1.py
@@ -1,3 +1,11 @@
-import lib.util
+from shlex import split
+import lib.util as util
+import subprocess
 
-print(get_default_iface_name_linux())
+command = "ip link set dev {iface} {status}"
+iface = util.get_default_iface_name_linux()
+
+args = split(command.format(iface=iface, status="down"))
+
+print(args)
+
