03f9e2abc73610c9f0e233eac7ff365b582afe23
Tony Situ <c2.tonyc2@gmail.com>
2018-02-21 00:31:55 +0000

    work on 2

--- advanced/scenarios/2.py
+++ advanced/scenarios/2.py
@@ -19,10 +19,22 @@ Delete the malicious entry and re add a entry to the proper default gateway i.e.
 # Memorize their current default gateway
 routing_info = util.get_default_routing_information()
 
+# Construct new bogus gateway
+gateway_bytes = routing_info.gateway.split(".")
+
+last_byte = int(gateway_bytes[-1])
+if last_byte < 250:
+    last_byte += 5
+else:
+    last_byte -= 5
+gateway_bytes[-1] = str(last_byte)
+ 
+new_gateway = ".".join(gateway_bytes)
+
 # Kill their default gateway routing rule
 command = "ip route delete default"
 subprocess.run(split(command))
 
 # Add a dummy entry
-command = "ip route add default via "
-iface = util.get_default_iface_name_linux()
+command = "ip route add default via {gateway}".format(gateway=new_gateway)
+subprocess.run(split(command))
