acc54328c52c7494e6ec839212655ed0211b65be
Tony Situ <c2.tonyc2@gmail.com>
2018-02-25 01:00:25 +0000

    work on 5 and 4

--- advanced/scenarios/4.py
+++ advanced/scenarios/4.py
@@ -2,43 +2,24 @@ from shlex import split
 import lib.util as util
 import subprocess
 
+
 """
 Scenario Overview:
-We will be simulating spoofed DNS responses using dnsmasq, a tool that will let us run a nameserver on our local machine.  Once the nameserver is up, we will configure certain hostnames to point to a blackhole server.  Additionally, we will edit /etc/resolv.conf to prioritize the local nameserver when it comes to resolving hostnames via lookup tools like dig.
+Simulating being unable to perform DNS lookup at all.  This can be done by simply point the default resolver to be some bogus address.
 
 Triage:
-The student should use a lookup tool like dig to verify that they are receiving DNS responses for their queries.  However, since these entries are spoofed the bindings are bogus.
-They should cross validate this result -- one good way to do this is to have the lookup tool use an alternative nameserver to issue the same query and notice that connections to this IP are valid.  Another option is to perform a reverse DNS lookup via another DNS server.  The key here is that you need to cross validate the IP in the DNS response in order to confirm that it's invalid.
+The student should note that name translation fails.  This can be done by first showing that running a tool with a domain name fails i.e. ping google.com.  Next they can find out that running that same tool with a static IP might work, a good IP to use is one that is public and well known i.e. google DNS at 8.8.8.8.  Additionally, they can try to use a lookup tool like dig or nslookup and discover that they fail.
 
 Solution:
-Since the current DNS server has a malicious record there are multiple ways to solve this issue.  One can place a static entry in /etc/hosts if the IP of the destination in question doesn't change.  Additionally, you can modify the default DNS server to be something more 'trustworthy', Google DNS at 8.8.8.8 for example.  This can be done by adding an entry in /etc/resolv.conf, mind the order of the entry since this affects its priority.
+This can be solved by setting resolv.conf to point at a valid nameserver.
 """
+
 RESOLV_FILE = "/etc/resolv.conf"
-ENTRY = "nameserver {local_ns}\n"
-DNSMASQ_HOST_FILE = "/etc/dnsmasq.hosts"
-DNSMASQ_CONFIG_FILE = "/etc/dnsmasq.conf"
 
-routing_entries = util.get_default_routing_information()
-default_entry = next((e for e in routing_entries if util.is_default_gateway(e)), None)
-default_iface_entry = next(
-    (e for e in routing_entries if not util.is_default_gateway(e) and e.iface == default_entry.iface),
-    None
-)
-
-ENTRY = ENTRY.format(local_ns=util.get_iface_ip_address(default_iface_entry.iface))
-
-# Write dnsmasq config file
-with open(DNSMASQ_HOST_FILE, "w"):
-    
-
-
-# Restart dnsmasq server
-command = "killall -9 dnsmasq"
-subprocess.run(split(command))
-
-# Add a line to resolv.conf to prioritize the local nameserver when executing DNS queries
+lines = []
+# comment out all lines in the resolv file
 with open(RESOLV_FILE, "r+") as resolv_file:
-    original = resolv_file.read()
-    file.seek(0, 0)
-    resolv_file.write(ENTRY + original)
-
+    for line in resolv_file:
+        lines.append("#" + line)
+    resolv_file.seek(0, 0)
+    resolv_file.write(''.join(lines))
